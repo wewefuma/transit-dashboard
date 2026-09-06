a648cbe85e04aead881957c8f560e3c1e5f93f9a
Angelo Buenaventura <alex@onyx.studio>
2026-09-06 19:09:47 +0800

    fix: vite optimizeDeps force true to resolve 504 timeout on maplibre-gl

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    force: true
+  },
   server: {
     port: 3000,
     strictPort: true
